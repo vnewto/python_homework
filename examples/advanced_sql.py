15f975d18448547a8889b7e10407fa9d15e1e87e
John R. McGarvey <johnmcgarvey55@gmail.com>
2025-02-12 16:19:20 -0500

    parameterized queries -- don't teach bad habits!

--- examples/advanced_sql.py
+++ examples/advanced_sql.py
@@ -4,7 +4,10 @@ try: # SQL operations can raise exceptions!
     
     # The with statement is helpful, because it ensures that the connection
     # is closed at the end, even if an exception occurs
-    with sqlite3.connect("./db/lesson.db",isolation_level='IMMEDIATE') as conn:    
+    with sqlite3.connect("./db/lesson.db",isolation_level='IMMEDIATE') as conn:
+        # The isolation_level means that a transaction is automatically opened.
+        # SQLite will open one automatically by default, but only with the
+        # first write request.  
         cursor = conn.cursor()
         conn.execute("PRAGMA foreign_keys = 1")
         # This does a SELECT and a subquery
@@ -42,34 +45,43 @@ try: # SQL operations can raise exceptions!
         stmt = """SELECT product_id FROM products ORDER BY price DESC LIMIT 5"""
         cursor.execute(stmt)
         result=cursor.fetchall()
-        print(result)
+        print("product list: ", result)
         products = result
 
         # Now we add the order, saving the returned order_id.  This example uses a parameterized
-        # statement.  It is not really necessary to use a parameterized statement when the
-        # values come from a trusted source, because no SQL injection could occur in that case.
+        # statement, as is best practice when putting variable values into SQL.
         stmt = f"""INSERT INTO orders (employee_id, customer_id) VALUES (?,?) RETURNING order_id"""
         cursor.execute(stmt,(employee_id, customer_id))
         result = cursor.fetchone()
         print(result)
         order_id = result[0]
         values = []
+        value_list = []
         for i in range(5):
-            values.append(f"({order_id}, {products[i][0]}, 10)")
-        values_string = ",".join(values)
-        
+            values.append("(?,?, 10)")
+            value_list.append(order_id)
+            value_list.append(products[i][0]) # The product ids we want
+        values_string = ",".join(values)      
         stmt = f"""INSERT INTO line_items (order_id, product_id, quantity) VALUES {values_string};"""
+        # This looks a little questionable, because we are using a formattted string for SQL.
+        # But the contents of the formatted string are created in the code, so it's safe --
+        # There is no potential of a security attack.  We do this to add the 5 line_item
+        # records with 1 insert statement.  For the actual values we use a parameterized
+        # statement.
         print(stmt)
-        cursor.execute(stmt)
+        cursor.execute(stmt, value_list)  
         conn.commit()
-        stmt=f"""SELECT l.line_item_id, l.quantity, p.product_name FROM line_items l JOIN products p ON 
-        l.product_id = p.product_id WHERE l.order_id = {order_id};"""
-        cursor.execute(stmt)
+        stmt="""SELECT l.line_item_id, l.quantity, p.product_name FROM line_items l JOIN products p ON 
+        l.product_id = p.product_id WHERE l.order_id = ?;"""
+        cursor.execute(stmt, (order_id,))
         result = cursor.fetchall()
         for row in result:
             print(row)
 except Exception as e:
-    print(f"An exception occurred: {e}")
+    print(f"An exception occurred: {type(e).__name__}")
+    raise e # we pass the exception on so that we can see the stack.
+    # this is a shortcut -- often one would catch specific exceptions and not pass
+    # them on.
 else:
     print("All SQL operations completed.")
     
